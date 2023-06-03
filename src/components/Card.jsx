3bb586107dbb83359866378771ad06380025a420
Vladimirs <vladimirribakov123@mail.ru>
2023-06-03 17:35:29 +0100

    making navigationg between each particular image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,17 @@
 import { useMemo } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-function Card({ path, title, createdAt, user }) {
+function Card({ path, title, createdAt, user, id }) {
+  const navigate = useNavigate();
+  const handleOnClick = () => {
+    navigate(`/images/${id}`, { state: { id } });
+  };
   const timestamp = useMemo(() => {
     const date = `${new Date(createdAt?.seconds * 1000)}`.split(' ');
     return `${date[1]} ${date[2]} ${date[3]}`;
   }, []);
   return (
-    <div className="col mb-5">
+    <div className="mb-5" onClick={handleOnClick}>
       <div className="card" style={{ width: '18rem' }}>
         <div
           style={{
--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ import Provider from './context/FirestoreContext';
 import AuthProvider, { useAuthContext } from './context/AuthContext';
 import Layout from './components/Layout';
 import StockImages from './components/StockImages';
+import Single from './components/Single';
 
 function AppRoutes() {
   const { currentUser } = useAuthContext();
   return (
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/images/:id" element={<Single />} />
       {currentUser && <Route path="/stockimages" element={<StockImages />} />}
     </Routes>
   );
